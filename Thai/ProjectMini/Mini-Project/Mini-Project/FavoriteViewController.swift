e907a5ae6e1bdd3e83758c8f37c0224767360d1d
Nguyen Xuan Thai <xuanthainguyen23@gmail.com>
2016-04-12 09:46:47 +0700

    again commit

---
+++ Thai/ProjectMini/Mini-Project/Mini-Project/FavoriteViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteViewController.swift
+//  Mini-Project
+//
+//  Created by Nguyen Xuan Thai on 4/11/16.
+//  Copyright © 2016 Thai. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBOutlet var favoriteTableView: UITableView!
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        return cell
+    }
+}
